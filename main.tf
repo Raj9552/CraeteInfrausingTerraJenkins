47bb0819b0f17967b6fddddf75d10eb27f3856cc
Raj9552 <137147928+Raj9552@users.noreply.github.com>
2023-09-17 14:37:21 +0530

    Create main.tf
    main.tf for infra creation

---
+++ main.tf
@@ -0,0 +1,64 @@
+provider "aws"{
+region=  var.aws_region
+}
+
+resource "aws-security_group" "my-security-group"{
+name= var.security_group
+description= "security group for ec2 instance"
+
+ingress{
+from_port= 8080
+to_port= 8080
+protocol= "tcp"
+cidr_blocks= ["0.0.0.0/0"]
+
+
+egress{
+from_port= 0
+to_port= 65535
+protocol= "tcp"
+cider_blocks= ["0.0.0.0./0"]
+}
+tags={
+name=" var.security_group"
+}
+}
+
+
+resource "aws-instance" "myfirstInstance"{
+ami= "ami-053b0d53c279acc90"
+key_name= var.key_name
+instance_type= var.instance_type
+security_group= [var.security_group]
+tags={
+name= var.tag_name
+}
+}
+
+
+resource "aws_eip" "myfirstEIP"{
+VPC = true
+instance= aws_instance.myfirstInstance.id
+tags= {
+name= "my-elastic-IP"
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
